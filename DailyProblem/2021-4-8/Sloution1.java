b337b67c146f552e5a864a5a60beccac62b5755a
LBingXiang <47679525+LBingXiang@users.noreply.github.com>
2021-04-08 14:50:39 +0800

    遍历 O(n)

--- DailyProblem/2021-4-8/Sloution1.java
+++ DailyProblem/2021-4-8/Sloution1.java
@@ -2,6 +2,7 @@ class Solution {
 
     //因为旋转数组时从后向前旋转的本题的时间复杂度
     //最坏为O(n) 最好为O(1), 所以平均为O(n/2)  -> O(n)
+    //思路简单，但是面试的时候，肯定不能要求使用这种方式
     public int findMin(int[] nums) {
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] < nums[0]) {
