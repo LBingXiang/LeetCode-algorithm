df13857176d442e5f489293e7d30f951a20b4eee
LBingXiang <47679525+LBingXiang@users.noreply.github.com>
2021-04-22 18:29:47 +0800

    dp + 动态规划的方式

---
+++ NoProblem/No494/Solution.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    /*
+        方法一：回溯算法
+        方法二：dp数组
+    */
+    public int findTargetSumWays(int[] nums, int target) {
+        if(nums.length == 0) {
+            return 0;
+        }
+
+        return subFunction(nums, 0, target);
+    }
+
+    //dp 数组
+    HashMap<String, Integer> dp = new HashMap<>();
+
+    private int subFunction(int[] nums, int i, int res) {
+        //base case
+        if(i == nums.length) {
+            if(res == 0) {
+                return 1;
+            }
+            return 0;
+        }
+
+        String key = i + "," + res;
+        if(dp.containsKey(key)) {
+            return dp.get(key);
+        }
+
+        int result = subFunction(nums, i+1, res - nums[i]) + subFunction(nums, i+1, res + nums[i]);
+        dp.put(key, result);
+
+        return result;
+    }
+}
